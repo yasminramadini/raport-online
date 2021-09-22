5e1b170729633da69445cc3269872f808c2d084c
Yasmin Ramadini <yasminramadini29@gmail.com>
2021-09-22 16:48:03 +0700

    migrasi table sekolah

---
+++ app/Database/Migrations/2021-09-22-094305_CreateTableSekolah.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableSekolah extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+          'id' => [
+            'type' => 'INT',
+            'constraint' => 11,
+            'auto_increment' => true,
+            'unsigned' => true
+            ],
+          'nama' => [
+            'type' => 'VARCHAR',
+            'constraint' => 100
+            ],
+          'alamat' => [
+            'type' => 'TEXT'
+            ],
+          'created_at' => [
+            'type' => 'DATETIME'
+            ],
+          'updated_at' => [
+            'type' => 'DATETIME'
+            ]
+          ]);
+          
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('sekolah', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('sekolah');
+    }
+}
--- app/Views/Admin/sekolah/data_sekolah.php
+++ app/Views/Admin/sekolah/data_sekolah.php
@@ -7,15 +7,18 @@
   <hr>
   <div class="row mt-5">
     <div class="col-lg-6">
+      
+      <p><i>*ketuk dua kali pada inputan untuk mengeditnya</i></p>
 
-      <form action="" method="post" enctype="multipart/form-data">
+      <form action="/admin/update_sekolah" method="post" enctype="multipart/form-data">
+        <?= csrf_field() ?>
         <div class="mb-3">
           <label for="nama">Nama Sekolah</label>
-          <input type="text" value="SMK Informatika" disabled id="nama" class="form-control">
+          <input type="text" value="SMK Informatika" readonly id="nama" class="form-control">
         </div>
         <div class="mb-3">
           <label for="alamat">Alamat Sekolah</label>
-          <textarea class="form-control" id="alamat" disabled>Jl. Silicon Valley, No 52, Sukabumi, Jawa Barat</textarea>
+          <textarea class="form-control" id="alamat" readonly>Jl. Silicon Valley, No 52, Sukabumi, Jawa Barat</textarea>
         </div>
         <div class="mb-3">
           <img src="<?= base_url('logo.png') ?>" width="100px" class="d-block img-thumbnail mb-3">
@@ -28,5 +31,25 @@
     </div>
   </div>
 </section>
+
+<script>
+  $(document).ready(function() {
+    $('#nama').on('dblclick', function() {
+      $(this).removeAttr('readonly')
+    })
+    
+    $('#nama').on('blur', function() {
+      $(this).attr('readonly', '')
+    })
+    
+    $('#alamat').on('dblclick', function() {
+      $(this).removeAttr('readonly')
+    })
+    
+    $('#alamat').on('blur', function() {
+      $(this).attr('readonly', '')
+    })
+  })
+</script>
 
 <?= $this->endSection() ?>
